2bf9c9b6447ec8b07418482371d7d321d1ff62f0
JinalBhanushali0628 <jinalbhanushali0628@gmail.com>
2020-11-29 21:51:11 -0500

    Updates

---
+++ precache-manifest.f06f0441521a1c1bd556b92fe3c64a74.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "268f36a220b312f9c292d46a6a5032af",
+    "url": "/ADIT-IEI-WEBSITE/index.html"
+  },
+  {
+    "revision": "4958f5d50791f9654734",
+    "url": "/ADIT-IEI-WEBSITE/static/css/main.5114291e.chunk.css"
+  },
+  {
+    "revision": "7a2655f585a666f79398",
+    "url": "/ADIT-IEI-WEBSITE/static/js/2.5bb4bfc7.chunk.js"
+  },
+  {
+    "revision": "dd207babaeb913476be46155cc504ef5",
+    "url": "/ADIT-IEI-WEBSITE/static/js/2.5bb4bfc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4958f5d50791f9654734",
+    "url": "/ADIT-IEI-WEBSITE/static/js/main.c992ef06.chunk.js"
+  },
+  {
+    "revision": "43073eaf312f285c7a6a",
+    "url": "/ADIT-IEI-WEBSITE/static/js/runtime-main.837dd80d.js"
+  },
+  {
+    "revision": "b10969c6615897d6ac80df7439475c06",
+    "url": "/ADIT-IEI-WEBSITE/static/media/event2.b10969c6.jpg"
+  },
+  {
+    "revision": "70f976fa046a4ca9ae0cddebd4edac21",
+    "url": "/ADIT-IEI-WEBSITE/static/media/happy.70f976fa.gif"
+  },
+  {
+    "revision": "ce672444c010099c4eac4ef248874cb9",
+    "url": "/ADIT-IEI-WEBSITE/static/media/iei_logo.ce672444.png"
+  },
+  {
+    "revision": "e0ea88c5db6ffde45479a44baa87da20",
+    "url": "/ADIT-IEI-WEBSITE/static/media/join.e0ea88c5.gif"
+  }
+]);
