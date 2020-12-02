61f5a9071059b7032aea12c43e83cddb1a39078b
Jinal Bhanushali <58743201+JinalBhanushali0628@users.noreply.github.com>
2020-12-02 11:55:30 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -27,15 +27,16 @@ function Main() {
   const handleClick = ()=>  setClick(!click);
   const closeClick = ()=>  setClick(false);
   const onMouseEnter = () => {
-    if(window.innerWidth < 960){
+    if(window.innerWidth < 1200){
       setDropdown(false);
+      setPhoneDropdown(false);
     }else{
       setDropdown(true);
       setPhoneDropdown(false);
     }
   };
   const onMouseLeave = () => {
-    if(window.innerWidth < 960){
+    if(window.innerWidth < 1200){
       setDropdown(false);
     }else{
       setDropdown(false);
@@ -49,34 +50,32 @@ function Main() {
   return <div className="main">
 
       <div className="">
-      <nav className="navbar">
+      <nav className="navbar navbar-expand-xl navbars">
         <div onClick={handleClick} className="menu-icon">
           <i className={click ? 'fa fa-times' : 'fa fa-bars'}></i>
         </div>
-        <img src={logo} alt="logo" className="navbar-logo"/>
-        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-         
-          <li className="nav-item">
-            <Link to="/" onClick={closeClick} className="nav-links">Home</Link>
+        <Link to="/ADIT-IEI-WEBSITE" onClick={closeClick} className="nav-link"><img src={logo} alt="logo" className="navbar-logo d-inline-block"/></Link>
+        <ul className={click ? 'nav-menus active navbar-nav mr-auto container' : 'nav-menus navbar-nav mr-auto container'}>
+          <li className="nav-item active nav-items">
+            <Link to="/ADIT-IEI-WEBSITE" onClick={closeClick} className="nav-link nav-links">Home</Link>
           </li>
-          <li className="nav-item"
+          <li className="nav-item  nav-items"
               onMouseEnter={onMouseEnter}
               onMouseLeave={onMouseLeave}
           >
-            <Link to="/event/presentEvent" onClick={window.innerWidth > 960 ? closeClick : phonedropdownOpen} className="nav-links"> Event <i className="fa fa-caret-down"></i></Link>
-            { dropdown && <Dropdown></Dropdown>
-              
-            }
-           
+            <Link to="/ADIT-IEI-WEBSITE/event/presentEvent" onClick={window.innerWidth > 1200 ? closeClick : phonedropdownOpen} className="nav-link nav-links" for="si"> 
+               Event &nbsp;
+               <i className="fa fa-caret-down"></i>
+            </Link>
+            { dropdown && <Dropdown id="si"></Dropdown> }
           </li>
-          <li> {phonedropdown && (
+           {phonedropdown && (
               (
-                <div className="">
+                <div>
                   <ul
                     onClick={handleClickbtn}
                     className={clickbtn ? 'Phonedropdown clicked' : 'Phonedropdown'}
                   >
-                  
                     {PhoneMenuItems.map((item, index) => {
                       return (
                         <li key={index} className="Phonedropdown-li">
@@ -93,48 +92,48 @@ function Main() {
                   </ul>
                 </div>
               ) 
-          )}</li>
-          <li className="nav-item">
-            <Link to="/gallery" onClick={closeClick} className="nav-links">Gallery</Link>
+          )}
+          <li className="nav-item nav-items">
+            <Link to="/ADIT-IEI-WEBSITE/gallery" onClick={closeClick} className="nav-link nav-links">Gallery</Link>
           </li>
-          <li className="nav-item">
-            <Link to="/ieiMembership" onClick={closeClick} className="nav-links">Members</Link>
+          <li className="nav-item nav-items">
+            <Link to="/ADIT-IEI-WEBSITE/ieiMembership" onClick={closeClick} className="nav-link nav-links">Member</Link>
           </li>
-           <li className="nav-item">
-            <Link to="/history" onClick={closeClick} className="nav-links">History</Link>
+           <li className="nav-item nav-items">
+            <Link to="/ADIT-IEI-WEBSITE/history" onClick={closeClick} className="nav-link nav-links">about us</Link>
           </li>
-          <li className="nav-item">
-            <Link to="/contact" onClick={closeClick} className="nav-links">Contact</Link>
+          <li className="nav-item nav-items">
+            <Link to="/ADIT-IEI-WEBSITE/contact" onClick={closeClick} className="nav-link nav-links">Core Team</Link>
           </li>
         </ul>
       </nav>
-      </div>
+    </div>
       <Switch>
-        <Route exact path="/" component={Home}></Route>
-        <Route path="/event/presentEvent" component={presentEvent}></Route>
-        <Route path="/event/pastEvent" component={pastEvent}></Route>
-        <Route path="/event/upcomingEvent" component={futureEvent}></Route>
-        <Route path="/gallery" component={Gallery}></Route>
-        <Route path="/ieiMembership" component={IEIMembership}></Route>
-        <Route path="/history" component={History}></Route>
-        <Route path="/contact" component={Contect}></Route>
+        <Route exact path="/ADIT-IEI-WEBSITE" component={Home}></Route>
+        <Route path="/ADIT-IEI-WEBSITE/event/presentEvent" component={presentEvent}></Route>
+        <Route path="/ADIT-IEI-WEBSITE/event/pastEvent" component={pastEvent}></Route>
+        <Route path="/ADIT-IEI-WEBSITE/event/upcomingEvent" component={futureEvent}></Route>
+        <Route path="/ADIT-IEI-WEBSITE/gallery" component={Gallery}></Route>
+        <Route path="/ADIT-IEI-WEBSITE/ieiMembership" component={IEIMembership}></Route>
+        <Route path="/ADIT-IEI-WEBSITE/history" component={History}></Route>
+        <Route path="/ADIT-IEI-WEBSITE/contact" component={Contect}></Route>
         <Route component={NotFound}></Route>
       </Switch>
     </div>
 }
 const PhoneMenuItems = [
     {
-        title : 'UpcomingEvent',
+        title : 'Upcoming Event',
         path : '/event/upcomingEvent',
         cName : 'Phonedropdown-li-link'
     },
     {
-        title : 'PresentEvent',
+        title : 'Present Event',
         path : '/event/presentEvent',
         cName : 'Phonedropdown-li-link'
     },
     {
-        title : 'PastEvent',
+        title : 'Past Event',
         path : '/event/pastEvent',
         cName : 'Phonedropdown-li-link'
     }
